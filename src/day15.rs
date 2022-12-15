18398c379d669263452836b0802daed867d54d8d
hfavereau <harold.favereau@etudiant.univ-rennes1.fr>
2022-12-15 13:11:00 +0100

    day 15 follow

--- src/day15.rs
+++ src/day15.rs
@@ -1,8 +1,11 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 use interval::interval_set::ToIntervalSet;
 use interval::IntervalSet;
 use gcollections::ops::*;
+use good_lp::{default_solver, SolverModel, variables};
+use itertools::Itertools;
 
 fn distance(p1: (i32,i32), p2: (i32,i32)) -> i32 {
     (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
@@ -13,6 +16,12 @@ struct Sensor {
     pos_x: i32,
     pos_y: i32,
     range_no_beacon: i32,
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct Beacon {
+    pos_x: i32,
+    pos_y: i32,
 }
 
 impl Sensor {
@@ -27,7 +36,7 @@ impl Sensor {
     }
 }
 
-fn parse(line: &str) -> (Sensor,i32) {
+fn parse(line: &str) -> (Sensor,Beacon) {
     let nbs = line.split(&[':',','])
         .map(|s| s.chars()
             .filter(|c| c.is_numeric() || *c == '-')
@@ -38,16 +47,17 @@ fn parse(line: &str) -> (Sensor,i32) {
         pos_x: nbs[0],
         pos_y: nbs[1],
         range_no_beacon: range,
-    },nbs[3])
+    },Beacon { pos_x: nbs[2], pos_y: nbs[3] })
 }
 
 pub fn day15() {
-    let mut file = File::open("./inputs/input_day15t.txt").expect("File not found");
+    let mut file = File::open("./inputs/input_day15.txt").expect("File not found");
     let mut data = String::new();
     file.read_to_string(&mut data)
         .expect("Error while reading file");
 
-    let row = 10;
+    let row = 2000000;
+    let max_coord = 4000000;
     let interval_size = data.lines()
         .map(|l| parse(l).0.get_no_beacon_row(row))
         .reduce(|accum, item| accum.union(&item))
@@ -55,8 +65,23 @@ pub fn day15() {
         .size();
     let sol1 = interval_size - data.lines()
         .map(parse)
-        .filter(|(_sensor,row_beacon)| *row_beacon == row)
-        .count() as u32;
-    println!("Solution 1 : {:?}", sol1)
+        .filter(|(_sensor,beacon)| beacon.pos_y == row)
+        .map(|(_sensor,beacon)| beacon.pos_y)
+        .collect::<HashSet<i32>>()
+        .len() as u32;
+
+    println!("Solution 1 : {:?}", sol1);
+
+    variables! {
+        vars:
+            0 <= x <= max_coord;
+            0 <= y <= max_coord;
+    };
+    let mut problem = vars.minimise(x)
+        .using(default_solver);
+    data.lines()
+        .map(|l| parse(l).0)
+        .for_each()
+    problem.add_constraint()
 
 }
